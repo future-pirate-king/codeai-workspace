3609c27a8867d755af6f001ff0811443f1a939fb
Sam David <samd948@gmail.com>
2019-07-12 00:59:46 +0530

    setting up admin and public apps

---
+++ apps/public/codeai/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'public-codeai',
+  preset: '../../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../../coverage/apps/public/codeai'
+};
